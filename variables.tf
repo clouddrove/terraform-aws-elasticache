c005e631d8eefdbf6ff98b1568541326c271f646
anmolnagpal <anmol@clouddrove.com>
2023-02-23 16:14:52 +0530

    use terraform letast version

--- variables.tf
+++ variables.tf
@@ -238,7 +238,7 @@ variable "kms_key_id" {
 
 variable "parameter_group_name" {
   type        = string
-  default     = "default.redis5.0"
+  default     = "default.memcached1.5"
   description = "The name of the parameter group to associate with this replication group. If this argument is omitted, the default cache parameter group for the specified engine is used."
 }
 
