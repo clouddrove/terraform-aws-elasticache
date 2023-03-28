e795e3d90c381294e01916a704b120309c475f89
Mamraj Yadav <mamraj.yadav@clouddrove.com>
2023-03-28 20:41:34 +0530

    added multi_az_enabled variable

--- variables.tf
+++ variables.tf
@@ -232,7 +232,7 @@ variable "kms_key_id" {
 
 variable "parameter_group_name" {
   type        = string
-  default     = "default.redis5.0"
+  default     = ""
   description = "The name of the parameter group to associate with this replication group. If this argument is omitted, the default cache parameter group for the specified engine is used."
 }
 
