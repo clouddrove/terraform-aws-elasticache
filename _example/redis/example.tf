30cbb8b9c443d9a36e0514744a69bdd58ec2d97c
anmolnagpal <anmol@clouddrove.com>
2023-05-04 21:04:39 +0530

    use terraform letast version

--- _example/redis/example.tf
+++ _example/redis/example.tf
@@ -57,11 +57,8 @@ module "redis" {
   security_group_ids         = [module.redis-sg.security_group_ids]
   availability_zones         = ["eu-west-1a", "eu-west-1b"]
   auto_minor_version_upgrade = true
-  multi_az_enabled           = false
-
-  number_cache_clusters = 2
-  retention_in_days     = 0
-  num_cache_clusters    = 2
+  num_cache_clusters         = 2
+  retention_in_days          = 0
 
   log_delivery_configuration = [
     {
