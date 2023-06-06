a8e02ecdcfbe4deb32b577ea401f7d6d54c5e87a
anmolnagpal <anmol@clouddrove.com>
2023-06-06 13:06:46 +0530

    feat: update and added resource description

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_security_group_rule" "egress" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = join("", aws_security_group.default.*.id)
 }
-#tfsec:ignore:aws-ec2-no-public-egress-sgr
+#defsec:ignore:aws-ec2-no-public-egress-sgr
 resource "aws_security_group_rule" "egress_ipv6" {
   count = (var.enable_security_group == true && length(var.sg_ids) < 1 && var.is_external == false) && var.egress_rule == true ? 1 : 0
 
