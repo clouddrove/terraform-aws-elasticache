dcbef428080e148517f39da19cb759bf7c327dd3
theprashantyadav <79893616+theprashantyadav@users.noreply.github.com>
2024-01-09 22:42:06 +0530

    feat: update tf.checks step name and tf and aws version (#64)
    Co-authored-by: anmolnagpal <anmol@clouddrove.com>

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.6.2"
+  required_version = ">= 1.6.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.22.0"
+      version = ">= 5.31.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.1"
+      version = ">= 3.6.0"
     }
   }
 }
