d62d70f798bbd384e38734d678df08e2133a1f17
ruchit-sharma09 <ruchit092003@gmail.com>
2026-05-05 22:25:01 +0530

    Update terraform providers version

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.65.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = ">= 5.31.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.0.0"
+      version = ">= 3.6.0"
     }
   }
 
